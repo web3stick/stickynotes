53f2181a29f1ceea134f277a5c43e19618298715
Nathan Renfroe <thesunshiningmin@gmail.com>
2026-04-10 12:29:02 -0500

    re manif

--- src/App.svelte
+++ src/App.svelte
@@ -1,3 +1,6 @@
+
+<!-- ============================================ -->
+<!-- ============================================ -->
 <main>
   <div class="content">
     <h1>Rsbuild with Svelte</h1>
@@ -5,24 +8,6 @@
   </div>
 </main>
 
-<style>
-.content {
-  display: flex;
-  min-height: 100vh;
-  line-height: 1.1;
-  text-align: center;
-  flex-direction: column;
-  justify-content: center;
-}
+<!-- ============================================ -->
+<!-- ============================================ -->
 
-.content h1 {
-  font-size: 3.6rem;
-  font-weight: 700;
-}
-
-.content p {
-  font-size: 1.2rem;
-  font-weight: 400;
-  opacity: 0.5;
-}
-</style>
