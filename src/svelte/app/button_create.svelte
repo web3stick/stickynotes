ebc1fc65b83173d5b99d5615a31327101adb0b75
Nathan Renfroe <thesunshiningmin@gmail.com>
2026-04-18 16:41:32 -0500

    re app

--- src/svelte/app/button_create.svelte
+++ src/svelte/app/button_create.svelte
@@ -10,4 +10,28 @@
 <!-- ============================================ -->
 <!-- ============================================ -->
 
-<style></style>
+<style>
+  #BUTTON_CREATE {
+    position: fixed;
+    bottom: 10px;
+    right: 10px;
+    width: 50px;
+    height: 50px;
+    background-color: var(--color-green);
+    color: #ffffff;
+    border: none;
+    border-radius: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 24px;
+    cursor: crosshair;
+    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+    transition: background-color 0.3s ease;
+    z-index: 5;
+  }
+
+  #BUTTON_CREATE:hover {
+    background-color: var(--color-blue);
+  }
+</style>
--- src/svelte/page_app.svelte
+++ src/svelte/page_app.svelte
@@ -1,3 +1,9 @@
+<script lang="ts">
+  import BUTTON_CREATE from "./app/button_create.svelte";
+  import BUTTON_OPEN_SETTINGS from "./app/button_open_settings.svelte";
+  import DIV_SETTINGS from "./app/div_settings.svelte";
+</script>
+
 <!-- ============================================ -->
 <!-- ============================================ -->
 
@@ -5,6 +11,11 @@
   <section>
     <h1>app</h1>
   </section>
+  <!-- buttons -->
+  <BUTTON_CREATE />
+  <BUTTON_OPEN_SETTINGS />
+  <!-- div -->
+  <DIV_SETTINGS/>
 </article>
 
 <!-- ============================================ -->
